ac31b4cad2fd4c585d9b1f23385d5c1d6156f60a
Noémie Huynh <huynhtieuquy@gmail.com>
2023-02-06 07:53:57 +0100

    learning DOM js

---
+++ DOM_ELEMENT/NotificationFacebook/1.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener('DOMContentLoaded', function(){
+     //khai bao bien can su dung trong app nay 
+     var tamgiac = document.getElementsByClassName('tamgiac');
+     var tamgiac = tamgiac[0];
+     var danhsach = document.getElementsByClassName('danhsach');
+     // su dung ham onclick va toggle class cho tal gia doi mau 
+
+     tamgiac.onclick = function(){
+          this.classList.toggle('tamgiacWhite');        
+          danhsach[0].classList.toggle('danhsach2');
+     };
+}, false);
+
+
---
+++ DOM_ELEMENT/classlist/1.js
@@ -0,0 +1,25 @@
+﻿ /* 3 fnction class:
+classList.remove(tenclass)
+
+classList.add(tenclass)
+
+classList.toggle(tenclass) (add va remove class)
+
+classList.remove() ( bo tat ca cac class)
+*/
+ var x1 = document.querySelectorAll('.n1');
+ console.log(x1);
+ 
+ x1[0].classList.remove('btn-outline-danger');
+ x1[0].classList.remove('btn');
+ x1[0].classList.add('btn-block');
+ x1[0].classList.add('btn-warning');
+
+
+ var x2 = document.querySelectorAll('.n3');
+ x2[0].classList.toggle('btn-block');
+ x2[0].classList.toggle('btn');
+
+ 
+
+ 
