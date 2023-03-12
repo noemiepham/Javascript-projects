48ae300422fc7b994b73dc31791f7281aa426ce3
Noémie Huynh <huynhtieuquy@gmail.com>
2023-03-12 07:45:07 +0100

    upload document learnung

---
+++ Document-fedu/044 fb tong quat 2/1.js
@@ -0,0 +1,44 @@
+﻿document.addEventListener("DOMContentLoaded",function(){
+	// bien su dung .declick
+	var nut = document.getElementsByClassName('declick');
+	var nd = document.getElementsByClassName('dehienthi');
+	for (var i = 0; i < nut.length; i++) {
+		nut[i].onclick = function(){
+			if(this.classList[1] == 'mautrang')	{ // click vao cai da hien thi roi 
+				
+				this.classList.remove('mautrang')		; // bo mau trang o chinh no
+				// 3 dong duoi la cho div cua doi tuong dc click an di
+				var ndhienra =  this.getAttribute('data-hienlen');
+				var phan_tu_hien_ra = document.getElementById(ndhienra);
+				phan_tu_hien_ra.classList.remove('ra');
+
+			}
+			else  // click vao cac cai con lai 
+			{
+				// cho tat ca bo mau trang di 
+				for (var k = 0; k < nut.length; k++) {
+					nut[k].classList.remove('mautrang');  
+				}
+
+				// doi tuong dc click (this) thanh mau trang
+				this.classList.toggle('mautrang');
+
+
+				//lay ve cai data-hienlien
+				var ndhienra =  this.getAttribute('data-hienlen');
+				var phan_tu_hien_ra = document.getElementById(ndhienra);
+
+				// cho tat ca cac div  .dehienthi khac an di 
+				for (var i = 0; i < nd.length; i++) {
+					nd[i].classList.remove('ra');
+				}
+
+				// cho div cua doi tuong dc click hien thi ra 
+				phan_tu_hien_ra.classList.toggle('ra');
+			}
+
+			
+			 		
+		}
+	}
+},false)
---
+++ Document-fedu/044 fb tong quat/1.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener("DOMContentLoaded",function(){
+	// bien su dung .declick
+	var nut = document.getElementsByClassName('declick');
+	var nd = document.getElementsByClassName('dehienthi');
+	for (var i = 0; i < nut.length; i++) {
+		nut[i].onclick = function(){
+			for (var k = 0; k < nut.length; k++) {
+				nut[k].classList.remove('mautrang');
+			}
+
+			this.classList.toggle('mautrang');
+			//lay ve cai data-hienlien
+			var ndhienra =  this.getAttribute('data-hienlen');
+			var phan_tu_hien_ra = document.getElementById(ndhienra);
+			for (var i = 0; i < nd.length; i++) {
+				nd[i].classList.remove('ra');
+			}
+			phan_tu_hien_ra.classList.toggle('ra');
+			 		
+		}
+	}
+},false)
