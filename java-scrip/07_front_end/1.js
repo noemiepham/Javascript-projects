88883d5a0182e1b558ca83a1b0bd14b57e669f15
Noémie Huynh <86301144+noemiepham@users.noreply.github.com>
2023-03-13 08:44:51 +0100

    Delete java-scrip directory

--- java-scrip/07_front_end/1.js
+++
@@ -1,9 +0,0 @@
-﻿var x = document.getElementsByClassName("card-title");
-
-for (var i = 0; i <x.length; i++)
-{
-     //console.log(i);
-
-     x[0].innerHTML = "Logo airbnb";
-}
-//console.log(x.length);
