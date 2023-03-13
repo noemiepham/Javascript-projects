93a6244593edffac3747bd2c32456604fbff7a2c
Noémie Huynh <huynhtieuquy@gmail.com>
2023-03-13 08:47:25 +0100

    change name

---
+++ Event_Handlers/NotificationFacebook/1.js
@@ -0,0 +1,38 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+  //bien su dung de.click
+  let nut = document.getElementsByClassName('declick');
+  let contentShow = document.getElementsByClassName('dehienthi');
+
+  for (let i = 0; i < nut.length; i++) {
+    nut[i].onclick = function () {
+      console.log(this.classList[1]);
+      if (this.classList[1] == 'mautrang') {
+        this.classList.remove('mautrang');
+
+        let ndhienra = this.getAttribute('data-hienlen');
+        let elementShow = document.getElementById(ndhienra);
+
+        elementShow.classList.remove('ra');
+
+      } else {
+        for (let k = 0; k < nut.length; k++) {
+          nut[k].classList.remove('mautrang');
+        }
+
+        this.classList.toggle('mautrang');
+
+        // show menju dropdown
+        let ndhienra = this.getAttribute('data-hienlen');
+        let elementShow = document.getElementById(ndhienra);
+        for (let l = 0; l < contentShow.length; l++) {
+          contentShow[l].classList.remove('ra');
+
+        }
+        elementShow.classList.toggle('ra');
+
+      }
+
+
+    }
+  }
+}, false);
---
+++ Event_Handlers/getAttribute/script.js
@@ -0,0 +1,17 @@
+﻿/*  Cach viet jquery su dung cac tham so truyen vao
+de co the ap dung voi cai the HTML khac ma co cung thong so */
+
+/* 
+thuoc tinh HTML5: data-content ( luu data trong html)
+Lay ve thong qua function getAttribut
+ */
+
+document.addEventListener('DOMContentLoaded', function(){
+     let nut = document.getElementsByClassName('btn');
+     for (let index = 0; index < nut.length; index++) {
+          const element = nut[index];
+          element.onclick = function(){
+               console.log(this.getAttribute('data-infor'));
+          }        
+     }
+}, false);
