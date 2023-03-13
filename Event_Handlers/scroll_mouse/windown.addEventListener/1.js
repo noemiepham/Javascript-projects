7055dab86d6cebc081066389c951e41d68014e84
Noémie Huynh <huynhtieuquy@gmail.com>
2023-03-13 23:35:24 +0100

    EventTarget.addEventListener()

---
+++ Event_Handlers/scroll_mouse/windown.addEventListener/1.js
@@ -0,0 +1,18 @@
+﻿ document.addEventListener('DOMContentLoaded', function(){
+     // chi in ra vi tri nguoi dung 1 lan 
+     let status = 'duoi300';
+     //bat su kien khi nguoi dung scroll
+     window.addEventListener('scroll', function(){
+     // vi tri cua nguoi dung
+     console.log(window.pageYOffset);
+     if (window.pageYOffset>300)
+     {
+          if(status == 'duoi300') {
+               console.log('lam gi..?? ');
+               status = 'tren300';
+          }
+     }
+     })
+
+}, false);
+ 
