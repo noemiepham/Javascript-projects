49a358632814e347d239f35b668562b97901ed97
Noémie Huynh <huynhtieuquy@gmail.com>
2023-03-14 01:28:47 +0100

    practic ex with js

--- Event_Handlers/scroll_mouse/menuScroll/1.js
+++ Event_Handlers/scroll_mouse/menuScroll/1.js
@@ -1,1 +1,21 @@
-﻿ 
+﻿ document.addEventListener('DOMContentLoaded', function(){
+     let status =  true;
+     let menuObject = document.querySelector('.menu');
+
+     window.addEventListener('scroll', function(){
+          if (window.pageYOffset > 300){
+               console.log('tren 300');
+               
+               if(status == true){
+                    menuObject.classList.add('SmallMenu')
+                    status = false;
+               } 
+          } else if (window.pageYOffset <= 300){
+               console.log('duoi 300');
+               
+               menuObject.classList.remove('SmallMenu')
+               status = true;
+          }
+     })
+ }, false);
+ 
